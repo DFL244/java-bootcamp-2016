b073286918468e1be2821fa01577281c110d620b
DFL244 <DFL244@users.noreply.github.com>
2016-02-24 10:58:53 -0500

    Adicion Codigo Singleton

---
+++ Projects/Test01/src/test01.java
@@ -0,0 +1,11 @@
+
+public class test01 {
+	int x, y,z;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a test i hope, it works");
+
+	}
+
+}
